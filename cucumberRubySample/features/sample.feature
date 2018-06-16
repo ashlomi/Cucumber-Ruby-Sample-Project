3b7f59b8113efe39c4bb33f824ebc0147af29193
anum <anumsharma09@gmail.com>
2018-06-15 19:23:32 -0700

    sample project

---
+++ cucumberRubySample/features/sample.feature
@@ -0,0 +1,9 @@
+Feature: practising for printing strings and numbers
+
+  @sample
+  Scenario: printing strings and numbers
+    Given I print strings to say hello to users
+    When I ask for user name
+    And I print the string "How are You!"
+    And  I calculate the sum of 2 numbers
+    Then I calculate the sum of "3" and "34"
---
+++ cucumberRubySample/features/step_definitions/sample_steps.rb
@@ -0,0 +1,20 @@
+Given(/^I print strings to say hello to users$/) do
+  puts "Please provide your Name"
+end
+
+When(/^I ask for user name$/) do
+  @name=STDIN.gets.chomp
+  puts "Hello #{@name}"
+end
+
+And(/^I print the string "([^"]*)"$/) do |str|
+  puts str + " " + @name
+end
+
+And(/^I calculate the sum of (\d+) numbers$/) do |arg|
+  puts sum=33+5
+end
+
+Then(/^I calculate the sum of "([^"]*)" and "([^"]*)"$/) do |arg1, arg2|
+  puts sum = arg1.to_f + arg2.to_f
+end
